8ce91249a4b2010d23a352c4d0a6a53f0a29886c
vigneshns68 <vickyviru332@gmail.com>
2022-09-09 20:20:19 +0530

    message for commit

---
+++ src/org/login8/ClientInfo.java
@@ -0,0 +1,43 @@
+package org.login8;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClientInfo {
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("C:\\Users\\ELCOT\\Desktop\\Vicky\\test.txt");
+		
+		file.createNewFile();
+		
+	System.out.println("filename"+file.getName());
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
